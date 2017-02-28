489c53abd1270f14d940db709cd847f9f2d449b8
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2017-02-27 20:56:02 -0800

    RNN with optimal parameters
    Tuned RNN parameters until optimal loss was achieved.

--- RNN.py
+++ RNN.py
@@ -92,6 +92,8 @@ def trainRNN():
 	model = Sequential()
 	model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
 	model.add(Dropout(0.2))
+	'''model.add(LSTM(256, return_sequences=True))
+	model.add(Dropout(0.2))'''
 	model.add(LSTM(256))
 	model.add(Dropout(0.2))
 	model.add(Dense(Y.shape[1], activation='softmax'))
@@ -104,7 +106,7 @@ def trainRNN():
 	callbacks_list = [checkpoint]
 
 	# fit the model
-	model.fit(X, Y, nb_epoch=20, batch_size=128, callbacks=callbacks_list)
+	model.fit(X, Y, nb_epoch=100, batch_size=128, callbacks=callbacks_list)
 	
 def generateRNN():
 	d = cmudict.dict()
@@ -125,6 +127,8 @@ def generateRNN():
 	model = Sequential()
 	model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
 	model.add(Dropout(0.2))
+	'''model.add(LSTM(256, return_sequences=True))
+	model.add(Dropout(0.2))'''
 	model.add(LSTM(256))
 	model.add(Dropout(0.2))
 	model.add(Dense(Y.shape[1], activation='softmax'))
@@ -193,7 +197,7 @@ def generateRNN():
 
 	print('DONE!')
 
-generateRNN()
+trainRNN()
 
 
 
